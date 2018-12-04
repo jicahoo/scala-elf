bee87e71ee0847d693504d4ea860d7a66e922383
jicahoo <jicahoo@163.com>
2018-12-04 17:55:57 +0800

    more code

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,16 +2,40 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 
 object App {
+
+
+  object WordSizeEnum extends Enumeration {
+    type WordSizeEnum = Value
+    val UNKNOWN, BIT32, BIT64 = Value
+  }
+
+  object EndianessEnum extends Enumeration {
+    type EndianessENum = Value
+    val UNKNOWN, LITTLE, BIG = Value
+  }
+
+  object OsEnum extends Enumeration {
+    type OsEnum = Value
+    //TODO: There are many other platforms
+    val Unknown, SystemV, HP_UX, NetBSD, Linux = Value
+  }
+
+
   def main(args: Array[String]): Unit = {
     println("Hello")
     var pwd = new File(".").getAbsolutePath
     println(pwd)
-    val byteArray = Files.readAllBytes(Paths.get("libssl.so.1.0.2"))
+    val filePath = "src/main/resources/libxml2.so.2.9.1"
+    val byteArray = Files.readAllBytes(Paths.get(filePath))
+    println(s"Byte size: ${byteArray.length}")
     println(byteArray(0) == 0x7f)
     println(byteArray(1) == 'E'.toInt) //E
     println(byteArray(2) == 'L'.toInt) //L
     println(byteArray(3) == 'F'.toInt)
-
+    val wordLen = byteArray(0x04)
+    println(WordSizeEnum.apply(wordLen))
+    println(EndianessEnum.apply(byteArray(0x5)))
+    println(OsEnum.apply(byteArray(0x7)))
   }
 
 }
