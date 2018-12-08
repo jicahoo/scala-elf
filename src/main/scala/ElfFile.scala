c14feb29324a2042e341a987029b20bfa1afc146
jicahoo <jicahoo@163.com>
2018-12-08 10:38:56 +0800

    refactor

--- src/main/scala/ElfFile.scala
+++ src/main/scala/ElfFile.scala
@@ -6,6 +6,11 @@ import SectionHeader._
 
 class ElfFile(val filePath: String) {
   private var _endian = EndianessEnum.LITTLE
+  private var _byteArray: Array[Byte] = _
+
+  private def asInt(offSetSizePair: OffSetSizePair): Int = {
+    asInt(_byteArray, offSetSizePair)
+  }
   private def asInt(bytetArray: Array[Byte], idx: Int, byteCnt: Int,
             endian: EndianessEnum.EndianessENum): Int = {
     assert(byteCnt <= 4)
@@ -31,45 +36,37 @@ class ElfFile(val filePath: String) {
   }
 
   def printSummary(): Unit = {
-    val byteArray = Files.readAllBytes(Paths.get(filePath))
+    _byteArray = Files.readAllBytes(Paths.get(filePath))
 
 
-    println(s"Byte size: ${byteArray.length}")
-    println(byteArray(0) == 0x7f)
-    println(byteArray(1) == 'E'.toInt) //E
-    println(byteArray(2) == 'L'.toInt) //L
-    println(byteArray(3) == 'F'.toInt)
-    val wordLenTag = asInt(byteArray, IdentMetaData.ET_CLASS)
+    println(s"Byte size: ${_byteArray.length}")
+    println(_byteArray(0) == 0x7f)
+    println(_byteArray(1) == 'E'.toInt) //E
+    println(_byteArray(2) == 'L'.toInt) //L
+    println(_byteArray(3) == 'F'.toInt)
+
+    val wordLenTag = asInt(IdentMetaData.ET_CLASS)
     val wordLen = WordSizeEnum.apply(wordLenTag)
-    println(wordLen)
-    val endian = EndianessEnum.apply(asInt(byteArray, IdentMetaData.ET_DATA))
+    val endian = EndianessEnum.apply(asInt(IdentMetaData.ET_DATA))
     _endian  = endian
-    println(endian)
-    val osType = OsEnum.apply(asInt(byteArray, IdentMetaData.ET_OSABI))
-    println(osType)
-    val elfType = ObjectFileTypeEnum.apply(asInt(byteArray, FileHeader.elfType))
-    println(elfType)
+    val osType = OsEnum.apply(asInt(IdentMetaData.ET_OSABI))
+    val elfType = ObjectFileTypeEnum.apply(asInt(FileHeader.elfType))
 
     assert(elfType == ObjectFileTypeEnum.ET_DYN)
-    var metaData: MetaData = null;
+    var metaData: MetaData = null
      wordLen match {
       case WordSizeEnum.BIT32 => metaData = new MetaData32
       case WordSizeEnum.BIT64 => metaData = new MetaData64
     }
 
     if (WordSizeEnum.BIT32 == wordLen) {
-      val progHdrOff = asInt(byteArray, metaData.phOff)
-      println(progHdrOff)
-      val phEntSize = asInt(byteArray, metaData.phEntSize)
-      val phEntNum = asInt(byteArray, metaData.phNum)
+      val progHdrOff = asInt(metaData.phOff)
+      val phEntSize = asInt(metaData.phEntSize)
+      val phEntNum = asInt(metaData.phNum)
 
-      val phType = asInt(byteArray, progHdrOff + 0x00, 4, endian)
-      println(ProgHeaderTypeEnum.apply(phType))
-
-      val sectHdrOff = asInt(byteArray, metaData.shOff)
-
-      val shEntSize = asInt(byteArray, metaData.shEntSize)
-      val shEntNum = asInt(byteArray, metaData.shNum)
+      val sectHdrOff = asInt(metaData.shOff)
+      val shEntSize = asInt(metaData.shEntSize)
+      val shEntNum = asInt(metaData.shNum)
 
       //TODO: Builder pattern? better?
       val fileHeader = FileHeader(
@@ -87,23 +84,27 @@ class ElfFile(val filePath: String) {
 
       println(fileHeader)
 
+      //first ph
+      val phType = asInt(_byteArray, progHdrOff + 0x00, 4, endian)
+      println(ProgHeaderTypeEnum.apply(phType))
+
       // Find the section .shstrtab
       val strTabSectHeaderIdx = sectHdrOff + (shEntNum - 1) * shEntSize
 
-      val shType = ShTypeEnum.apply(asInt(byteArray, strTabSectHeaderIdx + 0x4, 4, endian))
+      val shType = ShTypeEnum.apply(asInt(_byteArray, strTabSectHeaderIdx + 0x4, 4, endian))
       println(shType)
 
-      val sectionOffSet = asInt(byteArray, strTabSectHeaderIdx + 0x10, 4, endian)
+      val sectionOffSet = asInt(_byteArray, strTabSectHeaderIdx + 0x10, 4, endian)
       println(sectionOffSet)
-      val sectionSize = asInt(byteArray, strTabSectHeaderIdx + 0x14, 4, endian)
+      val sectionSize = asInt(_byteArray, strTabSectHeaderIdx + 0x14, 4, endian)
       println(sectionSize)
       (sectionOffSet until (sectionOffSet + sectionSize)).foreach(
         i => {
-          val c = byteArray(i)
+          val c = _byteArray(i)
           if (c == 0) {
             println()
           } else {
-            print(byteArray(i).toChar)
+            print(_byteArray(i).toChar)
           }
         }
       )
