0029e03587ed45166e3b2705d9ae9a34608a3606
jicahoo <jicahoo@163.com>
2018-12-08 16:33:25 +0800

    more code

--- src/main/scala/ProgramHeaderMetaData.scala
+++ src/main/scala/ProgramHeaderMetaData.scala
@@ -1,3 +1,32 @@
-trait ProgramHeaderMetaData {
+import java.time.OffsetDateTime
+
+abstract class ProgramHeaderMetaData {
+  def pType: OffSetSizePair
+  def pFlags: OffSetSizePair
+  def pOffset: OffSetSizePair
+  def pVirtualAddr: OffSetSizePair
+  def pPhysicalAddr: OffSetSizePair
+  def pFileSize: OffSetSizePair
+  def pMemSize: OffSetSizePair
+  def pALgin: OffSetSizePair
+
+}
+
+class ProgramHeaderMetaData32 extends ProgramHeaderMetaData {
+  override def pType: OffSetSizePair = OffSetSizePair(0x00, 4)
+
+  override def pOffset: OffSetSizePair = OffSetSizePair(0x04, 4)
+
+  override def pVirtualAddr: OffSetSizePair = OffSetSizePair(0x08, 4)
+
+  override def pPhysicalAddr: OffSetSizePair = OffSetSizePair(0x0C, 4)
+
+  override def pFileSize: OffSetSizePair = OffSetSizePair(0x10, 4)
+
+  override def pMemSize: OffSetSizePair = OffSetSizePair(0x14, 4)
+
+  override def pFlags: OffSetSizePair = OffSetSizePair(0x18, 4)
+
+  override def pALgin: OffSetSizePair = OffSetSizePair(0x1C, 4)
 
 }
