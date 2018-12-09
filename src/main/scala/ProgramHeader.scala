6e414ee6ed2743486081acb308f9b27258bab0d2
jicahoo <jicahoo@163.com>
2018-12-09 22:01:36 +0800

    more tries of scala relfections

--- src/main/scala/ProgramHeader.scala
+++ src/main/scala/ProgramHeader.scala
@@ -1,7 +1,11 @@
+import App.{Good, getClass}
 import FileHeader.EndianessEnum.EndianessENum
+import ProgramHeader.ProgHeaderTypeEnum.ProgHeaderTypeEnum
+import scala.reflect.runtime.{universe => ru}
 
-abstract class ProgramHeader {
 
+class ProgramHeader {
+  var pType: ProgHeaderTypeEnum  = _
 }
 
 object ProgramHeader {
@@ -17,4 +21,39 @@ object ProgramHeader {
     println(ProgHeaderTypeEnum.apply(phType))
     null
   }
+
+  def main(args: Array[String]): Unit = {
+    println("ProgramHeader")
+    val a = new ProgramHeader
+    val classMirror = ru.runtimeMirror(getClass.getClassLoader)
+    val classTest = classMirror.reflect(a)
+    val cls = ProgHeaderTypeEnum.getClass
+    val cSymbol = ru.runtimeMirror(cls.getClassLoader).classSymbol(cls)
+    //https://fair-jm.iteye.com/blog/2163746
+    //https://stackoverflow.com/questions/15008367/convert-class-to-universe-type-symbol
+    val classM = classMirror.reflectClass(cSymbol)
+    println(classM)
+    val fieldX = ru.typeOf[ProgramHeader].decl(ru.TermName("pType")).asTerm
+    println(fieldX)
+    println(fieldX.info.resultType.eq(ru.typeOf[ProgHeaderTypeEnum]))
+    println(ru.typeOf[ProgHeaderTypeEnum])
+    val x = ProgHeaderTypeEnum
+    val y = ru.typeOf[ProgHeaderTypeEnum]
+    val yDecls = y.decls
+    println(yDecls)
+
+    val phHeaderClass = ProgramHeader.getClass
+    val pType = phHeaderClass.getDeclaredFields
+    phHeaderClass.getDeclaredMethods.foreach(x => println(s"World: $x"))
+
+    pType.foreach(x => println(s"Hello:$x"))
+    phHeaderClass.getFields.foreach(x => println(s"Again: $x"))
+//
+    val xClass = x.getClass
+    val xInst = xClass.newInstance()
+    println(xInst.apply(0))
+  }
 }
+
+
+
